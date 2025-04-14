0253c8d1dfb040a834d318f9257d2803d682595c
Babak gholia <hoseinthegholi@gmail.com>
2025-04-14 17:21:59 +0330

    final structure
    including all function and classes neccessary for base structure of IOT project

--- IOT.py
+++ IOT.py
@@ -1,6 +1,9 @@
 import paho.mqtt.client as mqtt
 import time
+import adafruit_dht as dht
+from typing import Dict, Optional
 class Device:
+    '''main class for device management'''
     
     def __init__(self, topic, mqtt_broker, port = 1883):
         self.topic = topic
@@ -9,7 +12,7 @@ class Device:
         self.group=self.topic_list[1]
         self.device_type=self.topic_list[2]
         self.name=self.topic_list[3]
-        self.status_topic = f"{self.topic}/status"
+        self.status_topic = f"{self.topic}/status"  #prepared adress for requesting stat from broker
         
         self.mqtt_broker=mqtt_broker
         self.port=port
@@ -17,34 +20,36 @@ class Device:
         self.current_status = None  
         self.status_received = False  
         
-        self.mqtt_client.on_message = self._message
+        self.mqtt_client.on_message = self._message  #auto reciving device status on messages from broker
     def _message(self,message):
         stat = message.payload.decode()
-        if message.topic == f"{self.topic}/status":
+        if message.topic == self.status_topic:
             self.current_status = stat
             self.status_received = True
-            self.last_update = time.time() 
             
-    def connect(self):
-        self.mqtt_client.connect(self.mqtt_broker, self.port)
-        self.mqtt_client.loop_start()
-        self.mqtt_client.subscribe(self.status_topic)
-        print('Client connected and subscribed to status topic.')
-        
+    def connect(self):  #making a mqtt connection and keeping it alive for messages from server
+        try:
+            self.mqtt_client.connect(self.mqtt_broker, self.port)
+            self.mqtt_client.loop_start()
+            self.mqtt_client.subscribe(self.status_topic)
+            print(f'Device {self.name} connected and subscribed to status topic.')
+        except Exception as e:   
+            print(f"Connection error for {self.name}: {e}")
+            
     def disconnect(self):
         self.mqtt_client.loop_stop()
         self.mqtt_client.disconnect()
-        print('Client disconnected.')
+        print(f'Device {self.name} disconnected.')
        
-    def turn_on(self):
+    def turn_on(self):    #publishing a Turn_on request to broker
         self.mqtt_client.publish(self.topic, 'Turn_on')
-        print('Device turned on.')
+        print(f'Device {self.name} turned on.')
         
-    def turn_off(self):
+    def turn_off(self):   #publishing a Turn_off request to broker
         self.mqtt_client.publish(self.topic, 'Turn_off')
-        print('Device turned off.')
+        print(f'Device {self.name} turned off.')
         
-    def status(self,timeout=2):
+    def status(self, timeout :Optional[int]= 2) -> str:  #getting device status from broker manually if needed
         self.status_received = False
         self.mqtt_client.publish(self.topic, 'status_request')
         start_time = time.time()
@@ -53,142 +58,134 @@ class Device:
         if self.current_status is not None:
             return self.current_status
         return "unknown"
-    
-class Admin_panel(Device):
-    
-    def __init__(self, topic, mqtt_broker,port):
-        super().__init__(topic, mqtt_broker,port)
-        self.groups = {}
-        
-        
-    def create_gp(self, gp_name):
-        
-        if gp_name not in self.groups:
-            self.groups[gp_name] = []
-            print(f'group {gp_name} is created')
-        else:
-            print('your group name is existed already')        
-    
-    
-    def add_device(self,gp_name):
-        self.groups[gp_name].append(self.topic)
 
-    def create_multiple_devices(self,group_name,device_type,number_of_devices):
+class Sensor:
+    '''main class for sensor management'''
+    def __init__(self, topic:str, pin:int=4):
+        self.topic = topic
+        self.topic_list = self.topic.split('/')
+        self.location = self.topic_list[0]
+        self.group = self.topic_list[1]
+        self.sensor_type = self.topic_list[2]
+        self.name = self.topic_list[3]
+        self.pin = pin
+        self.last_reading_value = None
+    
+    def read_sensor(self):   #sending a read request to sensor
+        try:
+            humidity, temperature = dht.read_retry(dht.DHT22, self.pin)
+            if self.sensor_type == 'thermosets':
+                self.last_reading_value = temperature
+            else:
+                self.last_reading_value = humidity
+        except Exception as e:
+            print(f"Error reading sensor {self.name}: {e}")
+            self.last_reading_value = None
+        return self.last_reading_value
+class Admin_panel:
+    '''class to manage all devices and sensors'''
+    def __init__(self,gp: Optional[Dict] = None):
+        self.gp = gp or {}
+        self.devices: Dict[str, Device] = {} #saves topic as the adress and device as the instance of the main class to manage better
+        self.sensors: Dict[str, Sensor] = {}  #saves topic as the adress and sensor as the instance of the main class to manage better
+        self._initialize_devices()   #auto arranging devices and sensors on creating an instance of class
         
-        if group_name in self.groups:
-            for i in range(1,number_of_devices+1):
-                topic=f'home/{group_name}/{device_type}/{device_type}{i}'
-                new_device=Device(topic)
-                
-                self.add_device_to_group(group_name, new_device)
-                
-                #print...
-                
-
+    def _initialize_devices(self):  #sorting datas like the example in bottom
+        for location, groups in self.gp.items():
+            for group, device_types in groups.items():
+                for device_type, devices in device_types.items():
+                    for device_name, config in devices.items():
+                        topic = f"{location}/{group}/{device_type}/{device_name}"
+                        if device_type in ['termometers']:
+                            self.sensors[topic] = Sensor(topic=topic, pin=config['pin'])
+        
+                        else:
+                            self.devices[topic] = Device(topic=topic, mqtt_broker=config['mqtt'], port=config['port'])
+                            self.devices[topic].connect()
+                            
+    def create_gp(self, location: str, group: str, device_type: str, devices: Dict):  #creating a group if neccessary for arranging and prepration
+        if location not in self.gp:
+            self.gp[location] = {}
+        if group not in self.gp[location]:
+            self.gp[location][group] = {}
+        self.gp[location][group][device_type] = devices
+        self._initialize_devices()  
+    
+    def add_device(self, location: str, group: str, device_type: str, device_name: str, config: f'{Dict} must include mqtt_broker and port for device and pin for sensor'): #automaticly creates groups if not exists no need to def create_gp
+        if location not in self.gp:
+            self.gp[location] = {}
+        if group not in self.gp[location]:
+            self.gp[location][group] = {}
+        if device_type not in self.gp[location][group]:
+            self.gp[location][group][device_type] = {}
+        if device_name not in self.gp[location][group][device_type]:
+            self.gp[location][group][device_type][device_name] = config
+            topic = f"{location}/{group}/{device_type}/{device_name}"
+        else :
+            print('device already exists')
+        
+        if device_type in ['termometers']:
+            pin = config.get('pin', 4)
+            self.sensors[topic] = Sensor(topic, pin)
         else:
-            pass
-    def turn_on_devices_in_group(self,group_name):
-        
-        if group_name in self.groups:
-            
-            devices_list=self.groups[group_name]
-            
-            for device in devices_list:   
+            self.devices[topic] = Device(topic, config['mqtt'], config['port'])
+            self.devices[topic].connect()
+                    
+    def turn_on_devices_in_group(self, group_name: str):
+        for topic, device in self.devices.items():
+            if f"/{group_name}/" in topic:  #prevents simiraty mistakes in matching gp_name
                 device.turn_on()
             
-            
-            
-            
-        else:
-            pass
-    def trun_on_all(self):
-        '''hameye device haro roshan kone
-        tooo hame group ha 
-        
-        
-        '''
-        pass
-        
+    def turn_on_all(self):
+        for device in self.devices.values():
+            device.turn_on()
+    
     def turn_off_all(self):
-        '''hameye devcie haro khamosh kone'''
-        pass
-    
-        
-
-    def get_status_in_group(self,group_name):
-        
-        '''living_room y matn print mikone mige lamp1 on , klamp2 off , lamp3 ,..'''
-        pass
-    
-    
-    
-    
-    def get_status_in_device_type(self,device_type):
-        
-        ''' device=lamps --> tamame lamp haro status mohem nabashe tooye living rome kojas'''
-        pass
-#import Adafruity_DHT
-class sensor():
-    def __init__(self,topic,pin=100):
-        self.topic=topic
-        self.topic_list=self.topic.split('/')
-        self.location=self.topic_list[0]
-        self.group=self.topic_list[1]
-        self.sensor_type=self.topic_list[2]
-        self.name=self.topic_list[3]
-        
-    def create_sensor():
-        pass
-    def status_in_group():
-        pass
-    def read_sensor(self):
-        humidity, temperature=Adafruiy_DHT.read_retry(Adafruiy_DHT.DHT22,self.pin)
-        
-        if self.sensor_type=='thermosets':
-            return temperature
-        else:
-            return humidity
-        #import numpy
-        #a=numpy.uniform(20,25)
-        #return a
-        pass
-    def get_status_sensor_in_group(self,group_name):
-        
-        '''
-        
-        sensor haye yek goroh ro biad doone dooen status ro pas bde
-        
-        '''
-        pass
-        
-if __name__ == '__main__':
-    a1 = 'home/kitchen/lamps/lamp1'
-    '''a2 = Device(a1, mqtt_broker = 'broker.emqx.io')
-    a2.connect()
-    a2.ture_on()
-    a2.ture_off()'''
-    a2=sensor('home/paking/thermosets/thermo100',pin=431723689473674626392267349727)
-    a3=Admin_panel(a1, 'broker.emqx.io')
-    a3.create_gp('parking')
-    a3.add_device('parking')
-    print(a3.groups)
-
-
-'''def setup_gpio(self):
-        
-        #GPIO.setup(adad,GPIO.OUT)
-        #age lamp --> 17
-        #ag dar --> 27
-        #ag fan --> 22
-        
-        #---->
-        if self.device_type=='lamps':
-            GPIO.setup(17,GPIO.OUT)
-        
-        elif self.device_type=='doors':
-            GPIO.setup(27,GPIO.OUT)
+        for device in self.devices.values():
+            device.turn_off()
             
-        elif self.device_type=='fans':
-            GPIO.setup(22,GPIO.OUT)
+    def get_status_sensor_in_group(self, group_name: str) -> Dict[str, float]:
+        sensor_status = {}
+        for topic, sensor in self.sensors.items():
+            if f"/{group_name}/" in topic:
+                sensor_status[sensor.name] = sensor.read_sensor()
+        return sensor_status
             
-        #elif self.device_type..'''
+    def get_status_in_group(self, group_name: str) -> str:
+        status_list = []
+        for topic, device in self.devices.items():
+            if f"/{group_name}/" in topic:
+                status = device.status()
+                status_list.append(f"{device.name}: {status}")
+        return ", ".join(status_list)
+    
+    def get_status_in_device_type(self, device_type: str) -> str:
+        status_list = []
+        for device in self.devices.values():
+            if device.device_type == device_type:
+                status = device.status()
+                status_list.append(f"{device.name} ({device.location}/{device.group}): {status}")
+        return "\n".join(status_list)
+
+
+
+
+
+
+'''
+example_gp={'home':{
+                    'kitchen':{
+                                'lamps':{ 
+                                            'lamp1':{'mqtt':'any','port':1883},
+                                            'lamp2':{'mqtt':'any','port':1883}},
+                        
+                                'termometers':{ 
+                                            'termo1':{'pin':3},
+                                            'termo2':{'pin':2}},},
+                    'bedroom':{
+                                'lamps':{
+                                            'lamp1':{'mqtt':'any','port':1883},
+                                            'lamp2':{'mqtt':'any','port':1883}},
+                                'camera':{
+                                            'camera1':{'mqtt':'any','port':1883},
+                                            'camera2':{'mqtt':'any','port':1883}}}}}'''
